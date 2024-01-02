37af44ef3cc2286c52a907a203481bbf6c4838c5
Your Name <you@example.com>
2024-01-02 15:22:03 +0700

    completed

--- lab13_1.cpp
+++ lab13_1.cpp
@@ -17,3 +17,56 @@ int main(){
     cout << "\nMin = " << B[5];
     return 0;
 }
+
+void stat(const double A[],int N,double B[]){
+double sum = 0;
+for(int i = 0; i < N; i++ ){
+
+     sum += A[i];
+
+    B[0] = sum/N;
+
+}
+double summation = 0;
+for(int i = 0; i < N ; i++){
+
+    summation += pow(A[i] - B[0],2);
+    B[1] = sqrt(summation/N);
+
+}
+double sum2 = 1;
+for(int i = 0; i<N ; i++){
+
+    sum2 *= A[i];
+    B[2] = pow(sum2 , 1.0/N);
+
+}
+double sum3 = 0;
+for(int i = 0; i < N; i++){
+
+    sum3 += 1.0/A[i];
+    B[3] = N/sum3;
+}
+double max = A[0], min = A[0] ;
+for(int i = 0 ; i < N ; i++){
+
+if( max < A[i]){
+
+        max = A[i];
+
+    }
+
+    if(min > A[i]){
+
+        min = A[i];
+
+    }
+
+
+}
+
+    B[4] = max;
+    B[5] = min;
+
+
+}
